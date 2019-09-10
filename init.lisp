d8459ab712f9dc58c37a4a6eef3da3175f667f8a
vindarel <vindarel@mailz.org>
2019-09-10 12:53:25 +0200

    init: my-open-videos, old-reddit-hook

---
+++ init.lisp
@@ -0,0 +1,25 @@
+(in-package :next)
+
+(defun my-open-videos (filename)
+  "Open videos with mpv."
+  (handler-case (let ((extension (pathname-type filename)))
+                  (match extension
+                    ((or "flv" "mkv" "mp4")
+                     (uiop:launch-program (list "mpv" filename)))
+                    (_
+                     (next/file-manager-mode:open-file-function filename))))
+    (error (c) (log:error "Error opening pdf ~a: ~a" filename c))))
+
+(setf next/file-manager-mode:*open-file-function* #'my-open-videos)
+
+(defun old-reddit-hook (url)
+  "Enforce old reddit."
+  (let* ((uri (quri:uri url)))
+    (if (search "www.reddit" (quri:uri-host uri))
+        (progn
+          (setf (quri:uri-host uri) "old.reddit.com")
+          (let ((new-url (quri:render-uri uri)))
+            (log:info "Switching to old Reddit: ~a" new-url)
+            new-url))
+        url)))
+(add-to-default-list #'old-reddit-hook 'buffer 'load-hook)
